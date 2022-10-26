0f979bdc13bf3abce8d22819038c97fb992f1113
RANJITH <73814077+RanjithDevelopment@users.noreply.github.com>
2022-10-26 20:42:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express=require("express");
+const cors =require('cors');
+const amazonrouter=require('./Routers/amazonrouter');
+const mongo=require("./connect");
+const dotenv=require("dotenv");
+//here i called the database connection
+mongo.connect();
+dotenv.config();
+app.use(cors());
+const app=express();
+const {scraper}=require("./scraper/scraperamzon");
+app.use(express.json());
+app.use("/",(req,res,next)=>{
+    next();
+});
+//amazon
+app.use("/amazon",amazonrouter);
+//here initially called the scraper
+scraper();
+//to update the value i call scraper function for every 12 hours
+setInterval(()=>{scraper()},43200000);
+
+//console.log(process.env.PORT);
+app.listen(process.env.PORT);
+
+
+
